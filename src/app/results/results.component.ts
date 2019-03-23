0499a23328440711e5b5cd3ceae82d1af8bf0c99
Yuriy Granovskiy <yuriy.granovskiy@gmail.com>
2019-03-23 15:29:51 +0300

    Add more grades

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -10,16 +10,35 @@ export class ResultsComponent implements OnInit {
   correctCount: number;
   correctAndLongCount: number;
   incorrectCount: number;
-  canShowRapuntsel: boolean;
-
+  canShowGrade1: boolean;
+  canShowGrade2: boolean;
+  canShowGrade3: boolean;
+  canShowGrade4: boolean;
+  canShowGrade5: boolean;
+  
   constructor(private taskResultsService: TaskResultsService) {    
   }
 
   ngOnInit() {
     let results = this.taskResultsService.getResults();
+    let totalResults = results.length;
     this.correctCount = results.filter(result => result.isCorrect && !result.isLong).length;
     this.correctAndLongCount = results.filter(result => result.isCorrect && result.isLong).length;
     this.incorrectCount = results.filter(result => !result.isCorrect).length;
-    this.canShowRapuntsel = this.correctCount > results.length / 2;
+    if(totalResults === 0)    {
+      return;
+    }
+
+    if(this.correctCount === results.length) {
+      this.canShowGrade1 = true;
+    } else if (this.incorrectCount === 0 && this.correctCount > (totalResults / 4 * 3)){
+      this.canShowGrade2 = true;
+    } else if (this.incorrectCount === 0 && this.correctCount > totalResults / 2) {
+      this.canShowGrade3 = true;
+    } else if (this.correctCount > totalResults / 2) {
+      this.canShowGrade4 = true;
+    } else {
+      this.canShowGrade5 = true;
+    }    
   }
 }
